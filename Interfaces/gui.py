b73dcfc207f9c738ce8de128c2a4caaaeb45a8ed
Joe1651 <jonathan.duplantis@hotmail.com>
2024-10-01 15:29:54 -0400

    Update gui.py et assets

--- Interfaces/gui.py
+++ Interfaces/gui.py
@@ -9,7 +9,7 @@ from pathlib import Path
 from tkinter import Tk, Canvas, Entry, Text, Button, PhotoImage
 
 OUTPUT_PATH = Path(__file__).parent
-ASSETS_PATH = OUTPUT_PATH / Path(r"../Interfaces/assets/frame0")
+ASSETS_PATH = OUTPUT_PATH / Path(r"../Interfaces/assets/frame0/")
 
 
 def relative_to_assets(path: str) -> Path:
